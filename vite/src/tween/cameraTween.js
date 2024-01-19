53b43dcf73058e5c5bc0925ab82832cba5fb1b34
unknown <hengya03@gmail.com>
2024-01-18 22:00:01 -0600

    Adjusted the cameraTween.js by adjusting the camera and target positions. Also edited the NavBar to have its buttons move the camera to the correlating part of the room.

--- vite/src/tween/cameraTween.js
+++ vite/src/tween/cameraTween.js
@@ -12,18 +12,17 @@ export default function cameraTween(camera, target, selectedTarget, func) {
   const zoomInSpeed = 2000;
   const zoomOutSpeed = 2000;
   func(false);
- 
+
   // const dispatch = useDispatch();
   // const setIsAnimationFinished = (bool) => {
   //   dispatch({ type: 'SET_IS_ANIMATION_FINISHED', payload: bool });
   //   return;
   // }
 
-
-  const tweenFreeLook = () => {
+  const tweenMe = () => {
 
     const tweenCamera = new TWEEN.Tween({ x: camera.position.x, y: camera.position.y, z: camera.position.z })
-      .to({ x: 10, y: 6, z: 10 }, zoomOutSpeed)
+      .to({ x: -1.4, y: 4, z: 0.15 }, zoomInSpeed)
       .onUpdate((coords) => {
         camera.position.x = coords.x
         camera.position.y = coords.y
@@ -34,7 +33,7 @@ export default function cameraTween(camera, target, selectedTarget, func) {
       .delay(10);
 
     const tweenTarget = new TWEEN.Tween({ x: target.position.x, y: target.position.y, z: target.position.z })
-      .to({ x: 0, y: 2, z: 0 }, zoomOutSpeed)
+      .to({ x: -1.4, y: 2.5, z: -4 }, zoomInSpeed)
       .onUpdate((coords) => {
         target.position.x = coords.x
         target.position.y = coords.y
@@ -44,15 +43,80 @@ export default function cameraTween(camera, target, selectedTarget, func) {
       .repeat(0)
       .delay(10);
 
+
     tweenCamera.start();
     tweenTarget.start();
-    tweenCamera.onComplete(()=>{func(true)});
+    tweenCamera.onComplete(() => { func(true) });
   }
+
+
+  const tweenBooks = () => {
+
+    const tweenCamera = new TWEEN.Tween({ x: camera.position.x, y: camera.position.y, z: camera.position.z })
+      .to({ x: -2.5, y: 5, z: -.83 }, zoomInSpeed)
+      .onUpdate((coords) => {
+        camera.position.x = coords.x
+        camera.position.y = coords.y
+        camera.position.z = coords.z
+      })
+      .easing(TWEEN.Easing.Exponential.InOut)
+      .repeat(0)
+      .delay(10);
+
+    const tweenTarget = new TWEEN.Tween({ x: target.position.x, y: target.position.y, z: target.position.z })
+      .to({ x: -6, y: 1.5, z: -.83 }, zoomInSpeed)
+      .onUpdate((coords) => {
+        target.position.x = coords.x
+        target.position.y = coords.y
+        target.position.z = coords.z
+      })
+      .easing(TWEEN.Easing.Exponential.InOut)
+      .repeat(0)
+      .delay(10);
+
+
+    tweenCamera.start();
+    tweenTarget.start();
+    tweenCamera.onComplete(() => { func(true) });
+  }
+
+
+  const tweenTrophies = () => {
+
+    const tweenCamera = new TWEEN.Tween({ x: camera.position.x, y: camera.position.y, z: camera.position.z })
+      .to({ x: -2.5, y: 5, z: -.83 }, zoomInSpeed)
+      .onUpdate((coords) => {
+        camera.position.x = coords.x
+        camera.position.y = coords.y
+        camera.position.z = coords.z
+      })
+      .easing(TWEEN.Easing.Exponential.InOut)
+      .repeat(0)
+      .delay(10);
+
+    const tweenTarget = new TWEEN.Tween({ x: target.position.x, y: target.position.y, z: target.position.z })
+      .to({ x: -6, y: 1.5, z: -.83 }, zoomInSpeed)
+      .onUpdate((coords) => {
+        target.position.x = coords.x
+        target.position.y = coords.y
+        target.position.z = coords.z
+      })
+      .easing(TWEEN.Easing.Exponential.InOut)
+      .repeat(0)
+      .delay(10);
+
+
+    tweenCamera.start();
+    tweenTarget.start();
+    tweenCamera.onComplete(() => { func(true) });
+  }
+
+
 
   const tweenLaptop = () => {
 
     const tweenCamera = new TWEEN.Tween({ x: camera.position.x, y: camera.position.y, z: camera.position.z })
-      .to({ x: -3, y: 2.5, z: 2.76 }, zoomInSpeed)
+      .to({ x: -1.5, y: 4.5, z: 2.76 }, zoomInSpeed)
       .onUpdate((coords) => {
         camera.position.x = coords.x
         camera.position.y = coords.y
@@ -63,7 +127,7 @@ export default function cameraTween(camera, target, selectedTarget, func) {
       .delay(10);
 
     const tweenTarget = new TWEEN.Tween({ x: target.position.x, y: target.position.y, z: target.position.z })
-      .to({ x: -4.9, y: 2.5, z: 2.76 }, zoomInSpeed)
+      .to({ x: -4, y: 2.5, z: 2.76 }, zoomInSpeed)
       .onUpdate((coords) => {
         target.position.x = coords.x
         target.position.y = coords.y
@@ -75,45 +139,16 @@ export default function cameraTween(camera, target, selectedTarget, func) {
 
     tweenCamera.start();
     tweenTarget.start();
-    tweenCamera.onComplete(()=>{func(true)});
+    tweenCamera.onComplete(() => { func(true) });
   }
 
 
-  const tweenShelf = () => {
+
+
+  const tweenSpeakers = () => {
 
     const tweenCamera = new TWEEN.Tween({ x: camera.position.x, y: camera.position.y, z: camera.position.z })
-      .to({ x: -2.5, y: 5, z: -.83}, zoomInSpeed)
-      .onUpdate((coords) => {
-        camera.position.x = coords.x
-        camera.position.y = coords.y
-        camera.position.z = coords.z
-      })
-      .easing(TWEEN.Easing.Exponential.InOut)
-      .repeat(0)
-      .delay(10);
-
-    const tweenTarget = new TWEEN.Tween({  x: target.position.x, y: target.position.y, z: target.position.z })
-      .to({ x: -6, y: 1.5, z: -.83}, zoomInSpeed)
-      .onUpdate((coords) => {
-        target.position.x = coords.x
-        target.position.y = coords.y
-        target.position.z = coords.z
-      })
-      .easing(TWEEN.Easing.Exponential.InOut)
-      .repeat(0)
-      .delay(10);
-
-
-    tweenCamera.start();
-    tweenTarget.start();
-    tweenCamera.onComplete(()=>{func(true)});
-  }
-
-
-  const tweenMe = () => {
-
-    const tweenCamera = new TWEEN.Tween({ x: camera.position.x, y: camera.position.y, z: camera.position.z })
-      .to({ x: -1.4, y: 3.25, z: 1 }, zoomInSpeed)
+      .to({ x: 1.25, y: 3.5, z: -1.5 }, zoomInSpeed)
       .onUpdate((coords) => {
         camera.position.x = coords.x
         camera.position.y = coords.y
@@ -124,7 +159,7 @@ export default function cameraTween(camera, target, selectedTarget, func) {
       .delay(10);
 
     const tweenTarget = new TWEEN.Tween({ x: target.position.x, y: target.position.y, z: target.position.z })
-      .to({ x: -1.4, y: 0.5, z: -5 }, zoomInSpeed)
+      .to({ x: 1.25, y: 4, z: -4.5 }, zoomInSpeed)
       .onUpdate((coords) => {
         target.position.x = coords.x
         target.position.y = coords.y
@@ -137,91 +172,60 @@ export default function cameraTween(camera, target, selectedTarget, func) {
 
     tweenCamera.start();
     tweenTarget.start();
-    tweenCamera.onComplete(()=>{func(true)});
+    tweenCamera.onComplete(() => { func(true) });
   }
 
 
-  const tweenDrawer = () => {
+  // const tweenBack = () => {
 
-    const tweenCamera = new TWEEN.Tween({ x: camera.position.x, y: camera.position.y, z: camera.position.z })
-    .to({ x: 1.25, y: 4.5, z: -1.5}, zoomInSpeed)
-    .onUpdate((coords) => {
-      camera.position.x = coords.x
-      camera.position.y = coords.y
-      camera.position.z = coords.z
-    })
-    .easing(TWEEN.Easing.Exponential.InOut)
-    .repeat(0)
-    .delay(10);
+  //   const tweenCamera = new TWEEN.Tween({ x: camera.position.x, y: camera.position.y, z: camera.position.z })
+  //     .to({ x: 6, y: 5, z: 6 }, zoomOutSpeed)
+  //     .onUpdate((coords) => {
+  //       camera.position.x = coords.x
+  //       camera.position.y = coords.y
+  //       camera.position.z = coords.z
+  //     })
+  //     .easing(TWEEN.Easing.Exponential.InOut)
+  //     .repeat(0)
+  //     .delay(10);
 
-    const tweenTarget = new TWEEN.Tween({ x: target.position.x, y: target.position.y, z: target.position.z })
-      .to({ x: 1.25, y: 1, z: -4.5 }, zoomInSpeed)
-      .onUpdate((coords) => {
-        target.position.x = coords.x
-        target.position.y = coords.y
-        target.position.z = coords.z
-      })
-      .easing(TWEEN.Easing.Exponential.InOut)
-      .repeat(0)
-      .delay(10);
-
-
-    tweenCamera.start();
-    tweenTarget.start();
-    tweenCamera.onComplete(()=>{func(true)});
-  }
+  //   const tweenTarget = new TWEEN.Tween({ x: target.position.x, y: target.position.y, z: target.position.z })
+  //     .to({ x: 0, y: 1, z: 0 }, zoomOutSpeed)
+  //     .onUpdate((coords) => {
+  //       target.position.x = coords.x
+  //       target.position.y = coords.y
+  //       target.position.z = coords.z
+  //     })
+  //     .easing(TWEEN.Easing.Exponential.InOut)
+  //     .repeat(0)
+  //     .delay(10);
 
 
-  const tweenBack = () => {
+  //   tweenCamera.start();
+  //   tweenTarget.start();
+  //   tweenCamera.onComplete(() => { func(true) });
+  // }
 
-    const tweenCamera = new TWEEN.Tween({ x: camera.position.x, y: camera.position.y, z: camera.position.z })
-    .to({x: 6, y: 5, z: 6  }, zoomOutSpeed)
-    .onUpdate((coords) => {
-      camera.position.x = coords.x
-      camera.position.y = coords.y
-      camera.position.z = coords.z
-    })
-    .easing(TWEEN.Easing.Exponential.InOut)
-    .repeat(0)
-    .delay(10);
-
-    const tweenTarget = new TWEEN.Tween({ x: target.position.x, y: target.position.y, z: target.position.z })
-      .to({ x: 0, y: 1, z: 0 }, zoomOutSpeed)
-      .onUpdate((coords) => {
-        target.position.x = coords.x
-        target.position.y = coords.y
-        target.position.z = coords.z
-      })
-      .easing(TWEEN.Easing.Exponential.InOut)
-      .repeat(0)
-      .delay(10);
-
-
-    tweenCamera.start();
-    tweenTarget.start();
-    tweenCamera.onComplete(()=>{func(true)});
-  }
-  
 
 
   switch (selectedTarget) {
-    case 'freeLook':
-      return tweenFreeLook();
+    case 'me':
+      return tweenMe();
+      break;
+    case 'books':
+      return tweenBooks();
+      break;
+    case 'trophies':
+      return tweenTrophies();
       break;
     case 'laptop':
       return tweenLaptop();
       break;
-    case 'shelf':
-      return tweenShelf();
-      break;
-    case 'me':
-      return tweenMe();
-      break;
-    case 'drawer':
-      return tweenDrawer();
+    case 'speakers':
+      return tweenSpeakers();
       break;
     default:
-      return tweenBack();
+      return tweenMe();
       break;
   }
 }
