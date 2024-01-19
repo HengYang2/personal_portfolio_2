48bb9fb2882744265a4218185e312c42f5d3f7ee
unknown <hengya03@gmail.com>
2024-01-18 23:55:41 -0600

    Got rid of the node_modules folder at the highest level because it was not needed there.

--- vite/src/components/ContactView/ContactView.jsx
+++ vite/src/components/ContactView/ContactView.jsx
@@ -7,7 +7,7 @@ import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import MusicNoteIcon from '@mui/icons-material/MusicNote';
 import MusicOffIcon from '@mui/icons-material/MusicOff';
 
-import { useDispatch} from "react-redux";
+import { useDispatch } from "react-redux";
 
 export default function ContactView() {
 
@@ -22,7 +22,7 @@ export default function ContactView() {
       <Paper variant='outlined' sx={{ bgcolor: 'grey', height: '25%', width: '65%', position: 'absolute', marginTop: '34%' }}>
         <Stack direction='row' spacing={2} sx={{ bgcolor: '', padding: '0.5%', height: '100%', width: '100%', position: 'absolute', display: 'flex', flexDirection: 'row', justifyContent: 'start' }}>
           <Paper variant='outlined' sx={{ bgcolor: '', height: '100%', aspectRatio: '1/1', display: 'flex', flexDirection: 'column', justifyContent: 'end', alignItems: 'center' }}>
-            <img src='../../../public/hengPicture.jpg' />
+            <img src='/hengPicture.jpg' />
             <Paper sx={{ position: 'absolute', marginBottom: '1%', padding: '0.25%' }}>
               <Typography variant='h1'>
                 Heng Yang
